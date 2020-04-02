7dd8f97eeaff4229e652d16017e4bead3115c1db
Justin Winger <justinwinger4@gmail.com>
2020-04-02 17:31:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,11 +3,11 @@ class CashRegister
   
   def initialize
     @total = 0
-    # employee_discount = discount
     cash_register_with_discount
   end
   
-  def cash_register_with_discount
+  def cash_register_with_discount(discount)
+    
     
   end
   
